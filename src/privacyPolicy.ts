1a5216103483952f456b2a33715f64716082f2f2
Mikko Tuohimaa <mikko.tuohimaa@vincit.fi>
2019-11-02 18:20:18 +0200

    Add privacy policy

---
+++ src/privacyPolicy.ts
@@ -0,0 +1,2 @@
+export const privacyPolicy = "This app doesn't collect any information whatsoever. Your privacy is respected to the max.";
+
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { getRandomMove } from './twisterWheel';
 import { dialogFlowResponse } from './dialogFlow';
+import { privacyPolicy } from './privacyPolicy';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req: any, res: any) => res.send(''));
+app.get('/privacyPolicy', (req: any, res: any) => res.send(privacyPolicy));
 app.get('/spin/text', (req: any, res: any) => res.send(getRandomMove()));
 app.post('/spin/dialogFlow', (req: any, res: any) => res.json(dialogFlowResponse(getRandomMove())));
 
